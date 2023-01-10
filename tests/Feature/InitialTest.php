9e8f0e4a643dc88b4a6ca58d4f9f3caefbf130a5
Mohammad Foroutan <m.foroutan26@yahoo.com>
2023-01-10 17:54:55 +0330

    add InitialTest class and phpunit config

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace mfrtn\Press\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class Initialtest extends TestCase
+{
+    /** @test */
+    public function my_first_test()
+    {
+        $this->assertTrue(true);
+    }
+}
