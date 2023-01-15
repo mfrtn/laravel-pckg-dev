028c0f4530c84cb7e0d157f78f7244e3e24f33d6
Mohammad Foroutan <m.foroutan26@yahoo.com>
2023-01-15 14:55:59 +0330

    Parsing the Date field using PHP Carbon Library

--- tests/Feature/PressFileParserTest.php
+++ tests/Feature/PressFileParserTest.php
@@ -2,6 +2,7 @@
 
 namespace mfrtn\Press\Tests;
 
+use Carbon\Carbon;
 use mfrtn\Press\PressFileParser;
 use Orchestra\Testbench\TestCase;
 
@@ -16,6 +17,17 @@ class PressFileParserTest extends TestCase
 
         $this->assertStringContainsString('title: My Title', $data[1]);
         $this->assertStringContainsString('description: Description here', $data[1]);
+        $this->assertStringContainsString('Blog post body here', $data[2]);
+    }
+
+    /** @test */
+    public function a_string_can_also_be_used_instead()
+    {
+        $pressFileParser = (new PressFileParser("---\ntitle: My Title\n---\nBlog post body here\n"));
+
+        $data = $pressFileParser->getData();
+
+        $this->assertStringContainsString('title: My Title', $data[1]);
         $this->assertStringContainsString('Blog post body here', $data[2]);
     }
 
@@ -37,6 +49,17 @@ class PressFileParserTest extends TestCase
 
         $data = $pressFileParser->getData();
 
-        $this->assertEquals("# Heading\n\nBlog post body here", $data['body']);
+        $this->assertEquals("<h1>Heading</h1>\n<p>Blog post body here</p>", $data['body']);
+    }
+
+    /** @test */
+    public function a_date_field_gets_parsed()
+    {
+        $pressFileParser = (new PressFileParser("---\ndate: November 26, 1989\n---\n"));
+
+        $data = $pressFileParser->getData();
+
+        $this->assertInstanceOf(Carbon::class, $data['date']);
+        $this->assertEquals('26/11/1989', $data['date']->format('d/m/Y'));
     }
 }
